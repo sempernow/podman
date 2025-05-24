ca295df62d7c2188e9828bba6acab3c5f62ad75b
dev <sempernow.com@gmail.com>
2025-05-23 20:35:41 -0400

    per-user/provision-podman-nologin.sh @ 2025-05-24T00:34Z

--- per-user/provision-podman-nologin.sh
+++ per-user/provision-podman-nologin.sh
@@ -24,9 +24,9 @@ local_user=$app-$domain_user
 local_group=$local_user
 
 grep -e "^$domain_user" /etc/passwd && {
-    echo "  This script creates a local account, '$app-$domain_user', for a *non-local* (AD domain) user."
-    echo "  However, this user, '$domain_user', is *local*."
-    echo "  Local users are advised to run (rootless) Podman from their existing local account."
+    echo "⚠  This script creates a local account, '$app-$domain_user', for a *non-local* (AD domain) user."
+    echo "    However, this user, '$domain_user', is *local*."
+    echo "    Local users are advised to run (rootless) Podman from their existing local account."
 
     exit 22
 }
@@ -56,7 +56,7 @@ id -un $local_user >/dev/null 2>&1 || {
     loginctl enable-linger $local_user
 }
 id -un $local_user >/dev/null 2>&1 || {
-    echo "ERR : FAILed @ useradd : $local_user does NOT EXIST."
+    echo "⚠ ERR : FAILed @ useradd : $local_user does NOT EXIST."
 
     exit 33
 }
@@ -64,8 +64,9 @@ id -un $local_user >/dev/null 2>&1 || {
 sudoers=podman-sudoers
 groups $domain_user |grep $sudoers     || usermod -aG $sudoers $domain_user
 ## Allow domain user access to home of its provisioned local user.
-groups $domain_user |grep $local_group || usermod -aG $local_group $domain_user
-#newgrp $local_group
+groups $domain_user |grep $local_group || usermod -aG $local_group $domain_user &&
+    echo "🚧 User '$domain_user' MUST LOGOUT/LOGIN to activate their membership in groups: '$sudoers' and '$local_group'."
+
 chown -R $local_user:$local_group $alt_home
 find $alt_home -type d -exec chmod 775 {} \+
 find $alt_home -type f -exec chmod 660 {} \+
@@ -74,22 +75,22 @@ find $alt_home -type f -exec chmod 660 {} \+
 restorecon -Rv $alt/home # Apply any resulting SELinux fcontext changes (again, just to be sure).
 ls -ZRhl $alt
 seVerify || {
-    echo " ERR : FAILed @ SELinux : semanage fcontext"
+    echo "⚠ ERR : FAILed @ SELinux : semanage fcontext"
 
     exit 66
 }
 
 grep -q $local_user /etc/subuid &&
     grep -q $local_group /etc/subgid || {
-        echo "  ERR : FAILed @ subids"
+        echo "⚠ ERR : FAILed @ subids"
 
         exit 77
     }
 
-podman run --rm --volume $alt_home:/mnt/home alpine sh -c '
-    echo $(whoami)@$(hostname -f)
+img=alpine
+podman run --rm --volume $alt_home:/mnt/home $img -- sh -c '
+    echo "🚀 Hello from $(whoami) in container $(hostname -f)!"
     umask 002
-    rm -f /mnt/home/test-write-access-*
     ls -hl /mnt/home
     touch /mnt/home/test-write-access-$(date -u '+%Y-%m-%dT%H.%M.%SZ')
     ls -hl /mnt/home
