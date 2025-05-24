3d906268813156d193df62848a040c1cccf61042
dev <sempernow.com@gmail.com>
2025-05-24 16:23:33 -0400

    per-user/podman-provision-sudoers.sh @ 2025-05-24T20:01Z

--- per-user/podman-provision-nologin.sh
+++ per-user/podman-provision-nologin.sh
@@ -14,7 +14,7 @@
 # - Idempotent
 ####################################################################
 [[ -n "${SUDO_USER:-}" ]] || {
-    echo "⚠  USAGE: sudo ${BASH_SOURCE##*/}"
+    echo "⚠  USAGE: sudo ${BASH_SOURCE##*/}" >&2
 
     exit 1
 }
@@ -32,15 +32,15 @@ local_user=$app-$domain_user
 local_group=$local_user
 
 id "$local_user" >/dev/null 2>&1 && grep -qe "^$local_user" /etc/passwd && {
-    echo "⚠  Local user '$local_user' already exists."
+    echo "⚠  Local user '$local_user' already exists." >&2
 
     exit 11
 }
 
 grep -qe "^$domain_user" /etc/passwd && {
-    echo "⚠  This script creates a local account, '$app-$domain_user', for a *non-local* (AD domain) user."
-    echo "    However, this user, '$domain_user', is *local*."
-    echo "    Local users are advised to run (rootless) Podman from their existing local account."
+    echo "⚠  This script creates a local account, '$app-$domain_user', for a *non-local* (AD domain) user." >&2
+    echo "    However, this user ($domain_user) is *local*." >&2
+    echo "    Local users are advised to run (rootless) Podman from their existing local account." >&2
 
     exit 22
 }
@@ -70,16 +70,16 @@ id "$local_user" >/dev/null 2>&1 || {
     loginctl enable-linger "$local_user"
 }
 id "$local_user" >/dev/null 2>&1 || {
-    echo "⚠  ERR : FAILed @ useradd : '$local_user' does NOT EXIST."
+    echo "⚠  ERR : FAILed @ useradd : '$local_user' does NOT EXIST." >&2
 
     exit 33
 }
 ## Allow domain user to self-provision.
-sudoers=podman-sudoers
+sudoers=podman-provisioners
 groups "$domain_user" |grep $sudoers     || usermod -aG "$sudoers" "$domain_user"
 ## Allow domain user access to home of its provisioned local user.
 groups "$domain_user" |grep $local_group || usermod -aG "$local_group" "$domain_user" &&
-    echo "🚧  User '$domain_user' MUST LOGOUT/LOGIN to activate their membership in groups: '$sudoers' and '$local_group'."
+    echo "🚧  User '$domain_user' MUST LOGOUT/LOGIN to activate their membership in groups: '$sudoers' and '$local_group'." >&2
 
 chown -R $local_user:$local_group $alt_home
 find $alt_home -type d -exec chmod 775 {} \+
@@ -89,18 +89,18 @@ find $alt_home -type f -exec chmod 660 {} \+
 restorecon -Rv $alt/home # Apply any resulting SELinux fcontext changes (again, just to be sure).
 ls -ZRhl $alt
 seVerify || {
-    echo "⚠  ERR : FAILed @ SELinux : semanage fcontext"
+    echo "⚠  ERR : FAILed @ SELinux : semanage fcontext" >&2
 
     exit 66
 }
 
 grep -q $local_user /etc/subuid || {
-    echo "⚠  ERR : FAILed to add subUID range for local user '$local_user'"
+    echo "⚠  ERR : FAILed to add subUID range for local user '$local_user'" >&2
     
     exit 77
 }
 grep -q $local_group /etc/subgid || {
-    echo "⚠  ERR : FAILed to add subGID range for local group '$local_group'"
+    echo "⚠  ERR : FAILed to add subGID range for local group '$local_group'" >&2
 
     exit 78
 }
--- per-user/podman-provision-sudoers.sh
+++ per-user/podman-provision-sudoers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ######################################################################
-# This script installs a group-scoped sudoers drop-in file
+# This script installs a group-scoped sudoers drop-in files
 # to provision users for Podman in rootless (per-user) mode: 
 # 
 # - Allows (AD) users to self provision a local proxy user.
@@ -11,27 +11,35 @@
 ######################################################################
 
 [[ $(whoami) == 'root' ]] || {
-    echo '  Must RUN AS root'
+    echo '❌  Must RUN AS root' >&2
 
     exit 11
 }
 
 app=podman
-scope=$app-sudoers
-self_provision=/usr/local/bin/$app-provision-nologin.sh
-sudoers=/etc/sudoers.d/$scope
-
-getent group $scope || groupadd -r $scope
 
 ## Allow (AD) user to self provision:
 ##  sudo $self_provision
-## Allow scoped-group sudoers to run binary as sudo:
+scope=$app-provisioners
+self_provision=/usr/local/bin/$app-provision-nologin.sh
+sudoers=/etc/sudoers.d/$scope
+getent group $scope || groupadd -r $scope
+tee $sudoers <<EOH
+Defaults:%$scope secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+%$scope ALL=(ALL) NOPASSWD: $self_provision
+EOH
+chown root:root $sudoers
+chmod 640 $sudoers
+
+## Limit the local proxy user to run only the podman binary in its declared environment:
 ##  sudo -u $app-$USER -- env ... $app ...
+scope=$app-local
+sudoers=/etc/sudoers.d/$scope
+getent group $scope || groupadd -r $scope
 tee $sudoers <<EOH
 Defaults:%$scope secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 Defaults:%$scope env_keep += "HOME XDG_RUNTIME_DIR DBUS_SESSION_BUS_ADDRESS"
-%$scope ALL=(ALL) NOPASSWD: $self_provision
-%$scope ALL=($app-*) NOPASSWD: /usr/bin/env, /usr/bin/$app
+%$scope ALL=(ALL) NOPASSWD: /usr/bin/env, /usr/bin/$app
 EOH
 chown root:root $sudoers
 chmod 640 $sudoers
